ef6c5e712eda738da27f69a3f4e6a7454fb19860
Renan Evangelista Pereira <RenanEvangelistaPereira@users.noreply.github.com>
2020-05-01 21:43:57 -0300

    Adicionar arquivos de projeto.

---
+++ GatewayTechMS/ChavesIntegracoes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace br.com.techms.Gateway
+{
+    public class ChavesIntegracoes
+    {
+        private String _chave;
+
+        public string Chave
+        {
+            get => _chave;
+            set
+            {
+                if (String.IsNullOrEmpty(value) || )
+                {
+                    _chave = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Valor fornecido é inválido");
+                }
+            }
+        }
+    }
+}
